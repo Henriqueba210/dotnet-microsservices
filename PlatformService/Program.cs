560369aeb435cdb9754bbb349f8c204efce65048
Henrique Barros de Almeida <henrique.balmeida@outlook.com>
2022-01-01 20:49:16 -0300

    feat: created DTOs and registered them using AutoMapper

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 
@@ -30,6 +31,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 PrepDb.PrepPopulation(app);
-
 
 app.Run();
