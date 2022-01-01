b79d7c2f34315e0d473af36bde7025a29565e7b9
Henrique Barros de Almeida <henrique.balmeida@outlook.com>
2022-01-01 20:55:26 -0300

    feat: created PlatformsController

---
+++ PlatformService/Controllers/PlatformController.cs
@@ -0,0 +1,20 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data.Interfaces;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _platformRepo;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepo platformRepo, IMapper mapper)
+        {
+            _platformRepo = platformRepo;
+            _mapper = mapper;
+        }
+    }
+}
