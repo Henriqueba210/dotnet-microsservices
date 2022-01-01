9143b6c20e0b56aa39e9484717005f3866bc7704
Henrique Barros de Almeida <henrique.balmeida@outlook.com>
2022-01-01 18:10:08 -0300

    feat: initial project strucutre

---
+++ PlatformService/Data/PlatformRepository.cs
@@ -0,0 +1,33 @@
+using PlatformService.Data.Interfaces;
+using PlatformService.Models;
+
+namespace PlatformService.Data {
+    public class PlatformRepo : IPlatformRepo
+    {
+        private readonly AppDbContext _context;
+
+        public PlatformRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+        public void CreatePlatform(Platform platform)
+        {
+            
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Platform GetPlatformById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
