c48f04925166302f39008e2b0beadc68d1f4a057
Henrique Barros de Almeida <henrique.balmeida@outlook.com>
2022-01-01 21:38:21 -0300

    feat: :sparkles: Created CreatePlatform route

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data.Interfaces;
 using PlatformService.Dtos;
+using PlatformService.Models;
 
 namespace PlatformService.Controllers
 {
@@ -34,6 +35,23 @@ namespace PlatformService.Controllers
                 return NotFound();
             }
             return Ok(_mapper.Map<PlatformReadDto>(platform));
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            if (!this.TryValidateModel(platformCreateDto))
+            {
+                return BadRequest(this.ModelState);
+            }
+
+            var platformModel = _mapper.Map<Platform>(platformCreateDto);
+            _platformRepo.CreatePlatform(platformModel);
+            _platformRepo.SaveChanges();
+
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+            return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id }, platformReadDto);
         }
     }
 }
